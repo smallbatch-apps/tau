05eea3858ca6d90df39e55105f5248561c8a8d49
Matt Burgess <matt@myclient.info>
2022-06-03 12:29:15 +0700

    Fix formatting and improve documentation

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 #!/usr/bin/env node
 
-const program = require('commander');
+const program = require("commander");
 
-const { compressFiles } = require('./compress');
-const { extractFiles } = require('./extract');
+const { compressFiles } = require("./compress");
+const { extractFiles } = require("./extract");
 
 program
-  .command('compress')
-  .description('Compress artifact files')
-  .option('-i, --input <inputDir>', 'Input directory', 'build/contracts')
-  .option('-o, --output <outputDir>', 'Output directory')
-  .option('-k, --keep <keysToKeep>', 'Keys that you want to keep')
-  .option('-a --ast-object', 'Do not store object created from the ast interface content')
-  .action(function(cmd){
+  .command("compress")
+  .description("Compress artifact files")
+  .option("-i, --input <inputDir>", "Input directory", "build/contracts")
+  .option("-o, --output <outputDir>", "Output directory")
+  .option("-k, --keep <keysToKeep>", "Keys that you want to keep")
+  .action(function (cmd) {
     compressFiles(cmd);
   });
 
 program
-  .command('extract <file> <key>')
-  .description('Extract element from truffle artifact')
-  .option('-c', 'Copy to clipboard (MacOS only)')
-  .option('-w', 'Strip whitespace')
-  .option('-e', 'Escape string')
-  .option('-s', 'Silent output - do not display value')
-  .action(function(fileName, key, cmd){
+  .command("extract <file> <key>")
+  .description("Extract element from truffle artifact")
+  .option("-c", "Copy to clipboard (MacOS only)")
+  .option("-w", "Strip whitespace")
+  .option("-e", "Escape string")
+  .option("-s", "Silent output - do not display value")
+  .action(function (fileName, key, cmd) {
     extractFiles(fileName, key, cmd);
   });
-
 
 program.parse(process.argv);
